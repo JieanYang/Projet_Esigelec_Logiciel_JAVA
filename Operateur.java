f1e29679a4c82e9395531ee502391ff91467f807
Diesnis <florian.diesnis@groupe-esigelec.org>
2017-05-04 11:14:46 +0200

    suite des class

--- Operateur.java
+++ Operateur.java
@@ -1,4 +1,29 @@
 
 public class Operateur {
 
+	protected  String nom;
+	protected int identifiant;
+
+	public Operateur(String Nom, int identifiant) {
+		this.nom = Nom;
+		this.identifiant = identifiant;
+
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public int getIdentifiant() {
+		return identifiant;
+	}
+
+	public void setIdentifiant(int identifiant) {
+		this.identifiant = identifiant;
+	}
+
 }
