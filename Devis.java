897c24ef95922a06ec4207ed507da3b343a71bb9
diesnisflorian <florian.diesnis@groupe-esigelec>
2017-05-09 22:31:19 +0200

    class devis completer

--- Devis.java
+++ Devis.java
@@ -1,4 +1,47 @@
 
 public class Devis {
+	private int id;
+	private String nomdevis;
+	private Client client;
+	private String categorie;
+	
+	public Devis(int id,String nomdevis,Client client, String categorie){
+		this.id = id;
+		this.nomdevis = nomdevis;
+		this.client = client;
+		this.categorie = categorie;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNomdevis() {
+		return nomdevis;
+	}
+
+	public void setNomdevis(String nomdevis) {
+		this.nomdevis = nomdevis;
+	}
+
+	public Client getClient() {
+		return client;
+	}
+
+	public void setClient(Client client) {
+		this.client = client;
+	}
+
+	public String getCategorie() {
+		return categorie;
+	}
+
+	public void setCategorie(String categorie) {
+		this.categorie = categorie;
+	}
 
 }
